b98767481d5aa5e7aac3f6f149098d0e0c071111
Tom <tomlynch021@gmail.com>
2025-08-18 12:10:10 +1000

    Fix remaining TypeScript errors: resolve empty interface and heading content issues

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,10 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-    // Extends all standard HTML textarea attributes
-  }
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
